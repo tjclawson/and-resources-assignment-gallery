9d9295d475249dd8ee297bf53ab7d2786ed21964
tjclawson <tjamesclawson@gmail.com>
2019-08-12 19:19:11 -0400

    Debugging add images programmatically section

--- app/src/main/java/com/example/galleryapp/MainActivity.kt
+++ app/src/main/java/com/example/galleryapp/MainActivity.kt
@@ -1,18 +1,64 @@
 package com.example.galleryapp
 
+import android.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val views = Array<ImageView>(11){ image_left }
+
+        views[0].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.abstract_abstract_expressionism_art_2505693))
+        views[1].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.adventure_automobile_classic_2533092))
+        views[2].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.aerial_photography_aerial_shot_aerial_view_2583847))
+        views[3].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.afterglow_beach_cliff_2555285))
+        views[4].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.alley_architecture_buildings_2526517))
+        views[5].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.architectural_design_architecture_bridge_2540653))
+        views[6].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.beautiful_breathtaking_canada_day_2526105))
+        views[7].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.bloom_blossom_flora_2567011))
+        views[8].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.close_up_colorful_colors_2529148))
+        views[9].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.clouds_coconut_trees_daylight_2486168))
+        views[10].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.colorful_colourful_houses_2501965))
+
+
+        val layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+
+        views.forEachIndexed { index, imageView ->
+            imageView.layoutParams = layoutParams
+            imageView.adjustViewBounds = true
+            imageView.setPadding(15, 15, 15, 15)
+            if(index >= 5){
+                right_column.addView(imageView)
+            } else {left_column.addView(imageView)}
+        }
+
+
+
+
 
 
     }
 
 
 }
+
+/*
+<style name="Images">
+        <item name="android:layout_width">match_parent</item>
+        <item name="android:layout_height">wrap_content</item>
+        <item name="android:padding">15dp</item>
+        <item name="android:adjustViewBounds">true</item>
+        <item name="android:src">@drawable/afterglow_beach_cliff_2555285</item>
+    </style>
+ */
